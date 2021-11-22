847e4bf3abec601ccd6d53e5e0553f300e89bbe2
Alexander <kotzspam@gmail.com>
2021-11-22 23:47:13 +0100

    Added function to shuffle an array

--- script.js
+++ script.js
@@ -156,3 +156,11 @@ const students = [
     image: "assets/images/students/wiktoria-dobrzewinska.jpg",
   },
 ];
+
+// Code to shuffle an array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
